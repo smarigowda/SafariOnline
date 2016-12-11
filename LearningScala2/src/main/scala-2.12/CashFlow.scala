533e54eba5530c27bbe636560fad716a07698705
Santosh <santosharakere@gmail.com>
2016-12-11 16:20:22 +0000

    complex match, structural matching

--- LearningScala2/src/main/scala-2.12/CashFlow.scala
+++ LearningScala2/src/main/scala-2.12/CashFlow.scala
@@ -7,6 +7,15 @@ case class CashFlow( amount: Double, currency: String, due: LocalDateTime ) {
 
 }
 
+case class Address(city: String, country: String) {
+
+}
+
+case class Person(name: String, age: Int, address: Address) {
+
+}
+
+
 object CashFlow {
   def main(args: Array[String]) = {
     println("main class")
@@ -17,5 +26,18 @@ object CashFlow {
       case CashFlow(v, "GBP", _) => println(s"UK Cash Flow... value = $v")
       case _ => println("default")
     }
+
+    val a1 = Address("London", "UK")
+    val a2 = Address("New York", "USA")
+
+    val p1 = Person("Santosh", 43, a1)
+    val p2 = Person("Lingaraju", 45, a2)
+
+    p1 match {
+      case Person(name, _, Address("London", _)) => {
+        println(s"$name lives in London")
+      }
+    }
+
   }
 }
