a65ba87f5abc3731d73f90c5ff4d42538524f9a8
Santosh <santosharakere@gmail.com>
2016-12-18 21:00:25 +0000

    sample 2

---
+++ GettingStartedWithApacheMavenRussGold/sample2/src/main/java/com/packtpub/maven/Hello.java
@@ -0,0 +1,18 @@
+package com.packtpub.maven;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ */
+public class Hello {
+
+    public static void main(String... args) {
+        BasicConfigurator.configure();
+        Logger logger = Logger.getLogger("com.packtpub");
+        logger.setLevel(Level.INFO);
+        logger.info("Hello, World!");
+    }
+}
