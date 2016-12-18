aec05e6dad12921b9ca862cc40344744bf5f1ba8
Santosh <santosharakere@gmail.com>
2016-12-18 21:10:39 +0000

    adding log4j dependency to fix build failure

--- GettingStartedWithApacheMavenRussGold/sample2/src/main/java/com/packtpub/maven/Hello.java
+++ GettingStartedWithApacheMavenRussGold/sample2/src/main/java/com/packtpub/maven/Hello.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
  * Hello world!
  */
 public class Hello {
-
     public static void main(String... args) {
         BasicConfigurator.configure();
         Logger logger = Logger.getLogger("com.packtpub");
