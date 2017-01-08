0221d3b5938747008742c68ae3e28de4be75b9be
Santosh <santosharakere@gmail.com>
2017-01-08 19:47:47 +0000

    add, lessThan and max, self reference

--- ScalaBook/src/main/scala-2.12/Rational.scala
+++ ScalaBook/src/main/scala-2.12/Rational.scala
@@ -1,8 +1,23 @@
 // n and d are class parameters
 class Rational(n: Int, d: Int) {
   require(d != 0)
-  println(s"created $n / $d")
-  override def toString = n +"/"+ d
+  val numer = n
+  val denom = d
+  println(s"created $numer / $denom")
+  override def toString = numer +"/"+ denom
+
+  def add(that: Rational): Rational = {
+    new Rational(n * that.denom + that.numer * d, d * that.denom )
+  }
+
+  def lessThan(that: Rational) = {
+    this.numer * that.denom < that.numer * this.denom
+  }
+
+  def lessThanV2(that: Rational) = numer * that.denom < that.numer * denom
+
+  def max(that: Rational) = if(this.lessThan(that)) that else this
+  def maxV2(that: Rational) = if(lessThan(that)) that else this
 }
 
 object Main extends App {
@@ -10,7 +25,21 @@ object Main extends App {
   // therefore, print its debug message whenever you create a new Rational instance
   val r1 = new Rational(3, 4)
   println(r1)
-  val r2 = new Rational(3, 0) // Illegal arguments exception
+  //  val r2 = new Rational(3, 0) // Illegal arguments exception
+
+  val onehalf = new Rational(1, 2)
+  val oneforth = new Rational(1, 4)
+  println(onehalf add oneforth)
+
+  println(oneforth lessThan onehalf)
+  println(onehalf.lessThan(oneforth))
+
+  println(onehalf.lessThanV2(oneforth))
+
+  println(onehalf.max(oneforth))
+  println(onehalf.maxV2(oneforth))
+
+
 }
 
 // By default, class Rational inherits the implementation of toString defined in class java.lang.Object
